a4d6ca3821252276012fd75fd630f7f2717e838c
Salih Zioui <www.cjb@gmx.de>
2024-04-10 21:12:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PrayerTimes
+
+This project provides a web app which displays islamic prayer times for different locations.
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      external: [
+        'tests/**/*'
+      ]
+    }
+  },
+  test: {
+    environment: 'happy-dom',
+    globals: false,
+    setupFiles: './tests/vitest.setup.ts',
+  },
+});
