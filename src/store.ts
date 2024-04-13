61d1736852dfd715dc2ac9b922c42f9d9f397b38
Salih Zioui <www.cjb@gmx.de>
2024-04-13 10:34:49 +0200

    add zustand and store

---
+++ src/store.ts
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+
+type Store = {
+	/** the coordinates of the location for which the times should be loaded */
+	coordinates: [number, number] | null;
+	/** a function which must be used to update the coordinates */
+	setCoordinates: (newCoordinates: [number, number] | null) => void;
+};
+
+export const useStore = create<Store>((setStore) => ({
+	coordinates: null,
+	setCoordinates: (newCoordinates) =>
+		setStore(() => ({ coordinates: newCoordinates })),
+}));
