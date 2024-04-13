46aae2a45f1917698174af6b8e4222b30b4eb506
Salih Zioui <www.cjb@gmx.de>
2024-04-13 12:13:45 +0200

    add first api

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GeoLocator } from "./components/geo-locator";
 
-export const App = () => <GeoLocator />;
+export const App = () => {
+	const queryClient = new QueryClient();
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<GeoLocator />
+		</QueryClientProvider>
+	);
+};
