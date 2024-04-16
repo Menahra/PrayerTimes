2c3dd5611ab454c8653024869e17fd646fd566b5
Salih Zioui <www.cjb@gmx.de>
2024-04-16 13:49:31 +0200

    increase coverage

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,27 @@
 import { render } from "@testing-library/react";
-import { describe, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { TestQueryWrapper } from "../../tests/TestQueryWrapper";
 import { App } from "../App";
 
 describe("App", () => {
+	global.navigator = {
+		...global.navigator,
+		geolocation: {
+			getCurrentPosition: vi.fn().mockImplementation((success) =>
+				Promise.resolve(
+					success({
+						coords: {
+							latitude: 51.1,
+							longitude: 45.3,
+						},
+					}),
+				),
+			),
+			clearWatch: vi.fn(),
+			watchPosition: vi.fn(),
+		},
+	};
+
 	it("renders a locator which initially shows a loading indicator", async () => {
 		const { getByRole } = render(
 			<TestQueryWrapper>
